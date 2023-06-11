386f17609068849a5b18284fafa201227fdb086c
M1kelos <128227021+M1kelos@users.noreply.github.com>
2023-06-11 18:28:03 +0200

    Add files via upload

---
+++ 05_Algorytm_Taylor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj wartość x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Podaj liczbę wyrazów n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double wynik = SzacujWartoscEx(x, n);
+        Console.WriteLine($"Szacowana wartość e^{x} = {wynik}");
+    }
+
+    static double SzacujWartoscEx(double x, int n)
+    {
+        double suma = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            suma += Math.Pow(x, i) / ObliczSilnie(i);
+        }
+
+        return suma;
+    }
+
+    static double ObliczSilnie(int n)
+    {
+        double silnia = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            silnia *= i;
+        }
+
+        return silnia;
+    }
+}
